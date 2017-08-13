a67a134d90d8fff7b4b8c9af6f40f86860528645
TheRandX <margitpav1@gmail.com>
2017-08-13 23:16:38 +0200

    Initial commit
    Basic API requests along with basic ui are working.

---
+++ LaunchCell.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchCell.swift
+//  Liftoff
+//
+//  Created by Pavol Margitfalvi on 13/08/2017.
+//  Copyright © 2017 Pavol Margitfalvi. All rights reserved.
+//
+
+import UIKit
+
+class LaunchCell: UITableViewCell {
+    
+    @IBOutlet weak var rocketName: UILabel!
+    @IBOutlet weak var missionName: UILabel!
+    @IBOutlet weak var date: UILabel!
+    
+}
